f1ddd73f6b3459b4145118fd030382df7347b56e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-25 05:07:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,10 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  counter = 0
-  max = array[0]
+  max = array[i]
   
-  while 
+  while i 
 end
 
 def find_min_value(array)
